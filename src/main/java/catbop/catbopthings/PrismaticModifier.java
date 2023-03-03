27fe5c7413f4a8ca90347b536c3c2913132919c2
dhyces <pokmon987@gmail.com>
2023-03-03 11:49:21 -0500

    Reformat

--- src/main/java/catbop/catbopthings/PrismaticModifier.java
+++ src/main/java/catbop/catbopthings/PrismaticModifier.java
@@ -13,26 +13,27 @@ import slimeknights.tconstruct.library.tools.nbt.ModDataNBT;
 import slimeknights.tconstruct.tools.TinkerModifiers;
 
 public class PrismaticModifier extends Modifier {
-  @Override
-  public void addVolatileData(@NotNull ToolRebuildContext context, int level, @NotNull ModDataNBT volatileData) {
-    TinkerModifiers.overslime.get().setFriend(volatileData);
-  }
 
-  @Override
-  public void onInventoryTick(IToolStackView tool, int level, Level world, LivingEntity holder, int itemSlot, boolean isSelected, boolean isCorrectSlot, @NotNull ItemStack stack) {
-    // update 1 times a second, but skip when active (messes with pulling bow back)
-    if (!world.isClientSide && holder.tickCount % 20 == 0 && holder.getUseItem() != stack) {
-      // check skylight
-      // note this may go negative, that is not a problem
-      int skylight = world.getBrightness(LightLayer.SKY, holder.blockPosition()) - world.getSkyDarken();
-      //adds 5 if the tool has the soul light (WIP name)
-      //skylight += tool.getModifierLevel(ModiferId) * 5;
-      if (skylight > 0) {
-        // has a 5% chance of restoring each second per level
-        if (stack.getDamageValue() < stack.getMaxDamage() && RANDOM.nextFloat() < (level * 0.01 * skylight)) {
-          ToolDamageUtil.repair(tool, 1);
-        }
-      }
+    @Override
+    public void addVolatileData(@NotNull ToolRebuildContext context, int level, @NotNull ModDataNBT volatileData) {
+        TinkerModifiers.overslime.get().setFriend(volatileData);
     }
-  }
+
+    @Override
+    public void onInventoryTick(IToolStackView tool, int level, Level world, LivingEntity holder, int itemSlot, boolean isSelected, boolean isCorrectSlot, @NotNull ItemStack stack) {
+        // update 1 times a second, but skip when active (messes with pulling bow back)
+        if (!world.isClientSide && holder.tickCount % 20 == 0 && holder.getUseItem() != stack) {
+            // check skylight
+            // note this may go negative, that is not a problem
+            int skylight = world.getBrightness(LightLayer.SKY, holder.blockPosition()) - world.getSkyDarken();
+            //adds 5 if the tool has the soul light (WIP name)
+            //skylight += tool.getModifierLevel(ModiferId) * 5;
+            if (skylight > 0) {
+                // has a 5% chance of restoring each second per level
+                if (stack.getDamageValue() < stack.getMaxDamage() && RANDOM.nextFloat() < (level * 0.01 * skylight)) {
+                    ToolDamageUtil.repair(tool, 1);
+                }
+            }
+        }
+    }
 }
